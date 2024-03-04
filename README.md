cdcb1ce5724ad7c84e6728ba3b0af09286d03d96
Abdullah al rafi <abdullah116632@gmail.com>
2024-03-04 16:14:34 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# E-commerce MERN project
+
+1. created express server -> express
+2. express error handdling middleware -> http-errors
+3. how to secure API -> xss-clean, express-rate-limit
---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+const xssClean = require("xss-clean");   // it helps to sanetize request data
+const rateLimit = require("express-rate-limit");
+
+
+
+const app = express()
+
+const rateLimiter = rateLimit({   /* this package is use for set limit of request , 
+                                    hare using this middleware we can sent only 5 request 
+                                    in one minute at particuler api endpoint where we use this middleware */
+    windowMs: 1*60*1000, // 1 minute,
+    max: 5,
+    message: "too many request from this IP, please try later"
+})
+
+
+app.use(rateLimiter)
+app.use(xssClean());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/test", (req, res) => {
+    res.status(200).send({
+        message: "api testing is working fine"
+    })
+})
+
+//client error handaling 
+app.use((req, res, next) => {
+      // first argument is status conde , second argument is error message.
+    next(createError(404, "route not found"));
+})
+
+// server error handaling -- all the error will come hare that not handled previous
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        success: false,
+        message: err.message
+    })
+})
+
+module.exports = app;
